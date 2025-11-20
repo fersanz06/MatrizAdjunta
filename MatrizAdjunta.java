fd2f8fa9c936021abf3b17d9541e16b7895b4c52
fersanz_06 <luisfernandosanchezbriseno052@gmail.com>
2025-11-20 16:40:39 -0600

    Commit 2

--- MatrizAdjunta.java
+++ MatrizAdjunta.java
@@ -11,22 +11,50 @@ public class MatrizAdjunta {
 
         double[][] m = new double[n][n];
 
-        System.out.println("Valores:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 m[i][j] = sc.nextDouble();
             }
         }
 
-        double[][] adj = new double[n][n];
+        double[][] cof = new double[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                adj[i][j] = Math.pow(-1, i + j + 1);
+                double d = determinante(sub(m, i, j));
+                cof[i][j] = Math.pow(-1, i + j) * d;
             }
         }
 
-        imprimir(adj);
+        imprimir(cof);
+    }
+
+    public static double[][] sub(double[][] m, int f, int c) {
+        int n = m.length;
+        double[][] r = new double[n - 1][n - 1];
+        int a = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == f) continue;
+            int b = 0;
+            for (int j = 0; j < n; j++) {
+                if (j == c) continue;
+                r[a][b] = m[i][j];
+                b++;
+            }
+            a++;
+        }
+        return r;
+    }
+
+    public static double determinante(double[][] m) {
+        int n = m.length;
+        if (n == 1) return m[0][0];
+        if (n == 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+        double d = 0;
+        for (int j = 0; j < n; j++) {
+            d += Math.pow(-1, j) * m[0][j] * determinante(sub(m, 0, j));
+        }
+        return d;
     }
 
     public static void imprimir(double[][] m) {
